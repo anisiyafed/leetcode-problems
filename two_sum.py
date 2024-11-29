4cd3d257a8dbfc1a9b4b445b6ea9aec21be0b72f
Anisiya Fedoseeva <capykaze@gmail.com>
2024-11-29 21:58:20 +0000

    optimal approach

--- two_sum.py
+++ two_sum.py
@@ -5,9 +5,12 @@ from typing import List
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)):
-            for j in range(i + 1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
+        seen = {}
+        
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in seen:
+                return [seen[complement], i]
+            seen[num] = i
         # If no pair
-        return []
+        raise ValueError("No two sum solution exists")
